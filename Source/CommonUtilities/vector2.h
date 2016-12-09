cd2e1274f7774f81191cd222b2cd014d366242e3
carlberyeur <carlcarlberger@gmail.com>
2016-12-09 19:08:08 +0100

    Rewrote Vector2 so we don't need to see Martins ugly comments etc anymore, plus nothing is defined in the class declaration anymore B-)

--- Source/CommonUtilities/vector2.h
+++ Source/CommonUtilities/vector2.h
@@ -1,118 +1,75 @@
 #pragma once
-#define VECTOR_TEMPLATE_DECL template<typename TYPE>
+
+#define self (*this)
 
 namespace CU
 {
 	template <typename TYPE>
 	class Vector3;
 
-
 	template<typename TYPE>
 	class Vector2
 	{
 	public:
-		// Construction
-		Vector2(void);
-		Vector2(const Vector2 &aVector2);
-		Vector2(const Vector3<TYPE>& aVector3);
-		template<typename U> explicit Vector2(const Vector2<U> &aVector2);
-		Vector2(const TYPE anX, const TYPE aY);
-		template<typename U> explicit Vector2(const U aX, const U aY);
+		inline Vector2();
+		inline Vector2(const Vector2& aVector2);
+		inline Vector2(const TYPE aX, const TYPE aY);
+		inline Vector2(const Vector3<TYPE>& aVector3);
+		template<typename U> inline explicit Vector2(const Vector2<U>& aVector2);
+		template<typename U> inline explicit Vector2(const U aX, const U aY);
 
-		// Arithmetic
-		friend Vector2 operator +(Vector2 aLeft, const Vector2 &aRight)
-		{
-			return aLeft += aRight;
-		}
-		friend Vector2 operator -(Vector2 aLeft, const Vector2 &aRight)
-		{
-			return aLeft -= aRight;
-		}
-		friend Vector2 operator *(Vector2 aLeft, const TYPE aRight)
-		{
-			return aLeft *= aRight;
-		}
-		friend Vector2 operator /(Vector2 aLeft, const TYPE aRight)
-		{
-			return aLeft /= aRight;
-		}
-		friend Vector2 operator -(Vector2 aRight)
-		{
-			aRight.x *= -1;
-			aRight.y *= -1;
-			return aRight;
-		}
+		inline Vector2& operator=(const Vector2& aRight);
+		template<typename U> inline Vector2& operator=(const Vector2<U>& aRight);
 
-		// Comparison
-		friend bool operator ==(const Vector2 &aLeft, const Vector2 &aRight)
-		{
-			if ((aLeft.x == aRight.x) && (aLeft.y == aRight.y))
-			{
-				return true;
-			}
-			else
-			{
-				return false;
-			}
-		}
-		friend bool operator !=(const Vector2 &aLeft, const Vector2 &aRight)
-		{
-			if ((aLeft.x == aRight.x) && (aLeft.y == aRight.y))
-			{
-				return false;
-			}
-			else
-			{
-				return true;
-			}
-		}
+		inline void Set(const TYPE aX, const TYPE aY);
+		template<typename U> inline void Set(const U aX, const U aY);
 
-		// Assignment
-		Vector2 &operator =(const Vector2 &aRight);
-		void Set(const TYPE aX, const TYPE aY);
+		inline Vector2& operator+=(const Vector2& aRight);
+		inline Vector2& operator-=(const Vector2& aRight);
+		inline Vector2& operator*=(const Vector2& aRight);
+		inline Vector2& operator/=(const Vector2& aRight);
 
-		template<typename U>
-		inline void Set(const U aX, const U aY);
-
-		// Compound assignment
-		Vector2 &operator +=(const Vector2 &aRight);
-		Vector2 &operator -=(const Vector2 &aRight);
-		Vector2 &operator *=(const TYPE aRight);
-		Vector2 &operator /=(const TYPE aRight);
-
-		// Info
-		TYPE Length(void) const;
-		TYPE Length2(void) const;
-		TYPE Dot(const Vector2<TYPE> &aRight) const;
-		//KROSSEN
-		TYPE Cross(Vector2<TYPE> aVector) const;
-		Vector2 GetNormalized(void) const;
-
-		const char* c_str() const;
-
-		// Manipulation
-		Vector2 &Normalize(void);
+		inline Vector2 operator+(const Vector2& aRight) const;
+		inline Vector2 operator-(const Vector2& aRight) const;
+		inline Vector2 operator*(const Vector2& aRight) const;
+		inline Vector2 operator/(const Vector2& aRight) const;
+		 
+		inline Vector2 operator-() const;
+		 
+		inline Vector2& operator*=(const TYPE aRight);
+		inline Vector2& operator/=(const TYPE aRight);
+		 
+		inline Vector2 operator*(const TYPE aRight) const;
+		inline Vector2 operator/(const TYPE aRight) const;
+		 
+		inline bool operator==(const Vector2& aRight) const;
+		inline bool operator!=(const Vector2& aRight) const;
+		 
+		inline TYPE Length() const;
+		inline TYPE Length2() const;
+		inline TYPE Dot(const Vector2& aRight) const;
+		 
+		inline TYPE Cross(Vector2<TYPE> aVector) const;
+		inline Vector2 GetNormalized() const;
+		inline Vector2& Normalize();
 
 		union
 		{
-			TYPE    x;
-			TYPE	myX;
-			TYPE	u;
-		};
-		union
-		{
-			TYPE    y;
-			TYPE	myY;
-			TYPE	v;
+#pragma warning(disable : 4201)
+			struct 
+			{
+				TYPE x;
+				TYPE y;
+			};
+#pragma warning(default : 4201)
+
+			TYPE vector[2];
 		};
 
-		static TYPE Dot(const Vector2 &aFirstVector, const Vector2 &aSecondVector);
-		static Vector2 Normalize(Vector2 aVector);
-
-		static const Vector2	Zero;
-		static const Vector2	UnitX;
-		static const Vector2	UnitY;
-		static const Vector2	One;
+		static const Vector2 Zero;
+		static const Vector2 UnitX;
+		static const Vector2 UnitY;
+		static const Vector2 One;
 	};
 
 	using Vector2c = Vector2<char>;
@@ -127,38 +84,36 @@ namespace CU
 	using Point2f = Vector2<float>;
 	using Point2d = Vector2<double>;
 
-	VECTOR_TEMPLATE_DECL
-		using Point2 = Vector2<TYPE>;
+	template<typename TYPE> using Point2 = Vector2<TYPE>;
 
 	template<typename TYPE> const Vector2<TYPE> Vector2<TYPE>::Zero(0, 0);
 	template<typename TYPE> const Vector2<TYPE> Vector2<TYPE>::UnitX(1, 0);
 	template<typename TYPE> const Vector2<TYPE> Vector2<TYPE>::UnitY(0, 1);
 	template<typename TYPE> const Vector2<TYPE> Vector2<TYPE>::One(1, 1);
 
-	//Constructors
-	VECTOR_TEMPLATE_DECL
-		Vector2<TYPE>::Vector2(void)
+	template<typename TYPE>
+	Vector2<TYPE>::Vector2() : x(static_cast<TYPE>(0)), y(static_cast<TYPE>(0))
 	{
-		myX = static_cast<TYPE>(0);
-		myY = static_cast<TYPE>(0);
-	}
-	VECTOR_TEMPLATE_DECL
-		Vector2<TYPE>::Vector2(const Vector2 &aVector2)
-	{
-		myX = aVector2.myX;
-		myY = aVector2.myY;
 	}
 
-	VECTOR_TEMPLATE_DECL
-		Vector2<TYPE>::Vector2(const Vector3<TYPE> &aVector3)
+	template<typename TYPE>
+	Vector2<TYPE>::Vector2(const Vector2& aVector2) : x(aVector2.x), y(aVector2.y)
 	{
-		myX = aVector3.myX;
-		myY = aVector3.myY; 
+	}
+
+	template<typename TYPE>
+	Vector2<TYPE>::Vector2(const TYPE aX, const TYPE aY) : x(aX), y(aY)
+	{
+	}
+
+	template<typename TYPE>
+	Vector2<TYPE>::Vector2(const Vector3<TYPE>& aVector3) : x(aVector3.x), y(aVector3.y)
+	{
 	}
 
 	template<typename TYPE>
 	template<typename U>
-	inline Vector2<TYPE>::Vector2(const Vector2<U> &aVector) : x(static_cast<TYPE>(aVector.x)), y(static_cast<TYPE>(aVector.y))
+	inline Vector2<TYPE>::Vector2(const Vector2<U>& aVector) : x(static_cast<TYPE>(aVector.x)), y(static_cast<TYPE>(aVector.y))
 	{
 	}
 
@@ -168,127 +123,208 @@ namespace CU
 	{
 	}
 
-	VECTOR_TEMPLATE_DECL
-		Vector2<TYPE>::Vector2(const TYPE aX, const TYPE aY)
+	template<typename TYPE>
+	Vector2<TYPE>& Vector2<TYPE>::operator=(const Vector2& aRight)
 	{
-		myX = aX;
-		myY = aY;
-	}
+		x = aRight.x;
+		y = aRight.y;
 
-	// Assignment
-	VECTOR_TEMPLATE_DECL
-		Vector2<TYPE>& Vector2<TYPE>::operator=(const Vector2 &aRight)
-	{
-		myX = aRight.myX;
-		myY = aRight.myY;
-		return *this;
+		return self;
 	}
 
 	template<typename TYPE>
 	template<typename U>
-		inline void Vector2<TYPE>::Set(const U aX, const U aY)
+	Vector2<TYPE>& Vector2<TYPE>::operator=(const Vector2<U>& aRight)
 	{
-		myX = static_cast<TYPE>(aX);
-		myY = static_cast<TYPE>(aY);
-	}
+		x = static_cast<TYPE>(aRight.x);
+		y = static_cast<TYPE>(aRight.y);
 
-	VECTOR_TEMPLATE_DECL
-	inline void Vector2<TYPE>::Set(const TYPE aX, const TYPE aY)
-	{
-		myX = aX;
-		myY = aY;
-	}
-
-	// Compound assignment
-	VECTOR_TEMPLATE_DECL
-		Vector2<TYPE>& Vector2<TYPE>::operator +=(const Vector2 &aRight)
-	{
-		myX += aRight.x;
-		myY += aRight.y;
-		return *this;
-	}
-	VECTOR_TEMPLATE_DECL
-		Vector2<TYPE>& Vector2<TYPE>::operator -=(const Vector2 &aRight)
-	{
-		myX -= aRight.myX;
-		myY -= aRight.myY;
-		return *this;
-	}
-	VECTOR_TEMPLATE_DECL
-		Vector2<TYPE>& Vector2<TYPE>::operator *=(const TYPE aRight)
-	{
-		myX *= aRight;
-		myY *= aRight;
-		return *this;
-	}
-	VECTOR_TEMPLATE_DECL
-		Vector2<TYPE>& Vector2<TYPE>::operator /=(const TYPE aRight)
-	{
-		myX /= aRight;
-		myY /= aRight;
-		return *this;
-	}
-
-	// Info
-	//
-	VECTOR_TEMPLATE_DECL
-		TYPE  Vector2<TYPE>::Length(void) const
-	{
-		return sqrt(((myX * myX) + (myY * myY)));
-	}
-	VECTOR_TEMPLATE_DECL
-		TYPE  Vector2<TYPE>::Length2(void) const
-	{
-		return ((myX * myX) + (myY * myY));
-	}
-	VECTOR_TEMPLATE_DECL
-		TYPE  Vector2<TYPE>::Dot(const Vector2<TYPE> &aRight) const
-	{
-		return ((myX * aRight.x) + (myY * aRight.y));
-	}
-	VECTOR_TEMPLATE_DECL
-		TYPE Vector2<TYPE>::Cross(Vector2<TYPE> aVector) const
-	{
-		return (x * aVector.y - y * aVector.x);
-	}
-	VECTOR_TEMPLATE_DECL
-		Vector2<TYPE> Vector2<TYPE>::GetNormalized(void) const
-	{
-		return Vector2<TYPE>(myX / Length(), myY / Length());
-	}
-
-	// Manipulation
-	VECTOR_TEMPLATE_DECL
-		Vector2<TYPE>& Vector2<TYPE>::Normalize(void)
-	{
-		TYPE length = Length();
-		myX /= length;
-		myY /= length;
-		return *this;
+		return self;
 	}
 
 	template<typename TYPE>
-	inline const char* Vector2<TYPE>::c_str() const
+	template<typename U>
+	inline void Vector2<TYPE>::Set(const U aX, const U aY)
 	{
-		static char locName[64];
-		memset(locName, 0, 64);
-		sprintf_s(locName, 64, "(%f, %f)", myX, myY);
-
-		return locName;
+		x = static_cast<TYPE>(aX);
+		y = static_cast<TYPE>(aY);
 	}
 
-	//STATICS
-	VECTOR_TEMPLATE_DECL
-		TYPE Vector2<TYPE>::Dot(const Vector2 &aFirstVector, const Vector2 &aSecondVector)
+	template<typename TYPE>
+	inline void Vector2<TYPE>::Set(const TYPE aX, const TYPE aY)
 	{
-		return ((aFirstVector.myX * aSecondVector.myX) + (aFirstVector.myY * aSecondVector.myY));
+		x = aX;
+		y = aY;
 	}
-	VECTOR_TEMPLATE_DECL
-		Vector2<TYPE> Vector2<TYPE>::Normalize(Vector2 aVector)
+
+	template<typename TYPE>
+	Vector2<TYPE>& Vector2<TYPE>::operator+=(const Vector2& aRight)
 	{
-		TYPE length = aVector.Length();
-		return Vector2<TYPE>(aVector.x / length, aVector.y / length);
+		x += aRight.x;
+		y += aRight.y;
+
+		return self;
+	}
+
+	template<typename TYPE>
+	Vector2<TYPE>& Vector2<TYPE>::operator-=(const Vector2& aRight)
+	{
+		x -= aRight.x;
+		y -= aRight.y;
+
+		return self;
+	}
+
+
+	template<typename TYPE>
+	Vector2<TYPE>& Vector2<TYPE>::operator*=(const Vector2& aRight)
+	{
+		x *= aRight.x;
+		y *= aRight.y;
+
+		return self;
+	}
+
+	template<typename TYPE>
+	Vector2<TYPE>& Vector2<TYPE>::operator/=(const Vector2& aRight)
+	{
+		x /= aRight.x;
+		y /= aRight.y;
+
+		return self;
+	}
+
+	template<typename TYPE>
+	Vector2<TYPE> Vector2<TYPE>::operator+(const Vector2& aRight) const
+	{
+		Vector2 left = self;
+		return left += aRight;
+	}
+
+	template<typename TYPE>
+	Vector2<TYPE> Vector2<TYPE>::operator-(const Vector2& aRight) const
+	{
+		Vector2 left = self;
+		return left -= aRight;
+	}
+
+	template<typename TYPE>
+	Vector2<TYPE> Vector2<TYPE>::operator*(const Vector2& aRight) const
+	{
+		Vector2 left = self;
+		return left *= aRight;
+	}
+
+	template<typename TYPE>
+	Vector2<TYPE> Vector2<TYPE>::operator/(const Vector2& aRight) const
+	{
+		Vector2 left = self;
+		return left /= aRight;
+	}
+
+	template<typename TYPE>
+	Vector2<TYPE> Vector2<TYPE>::operator-() const
+	{
+		Vector2 right = self;
+		
+		right.x = -right.x;
+		right.y = -right.y;
+
+		return right;
+	}
+
+	template<typename TYPE>
+	Vector2<TYPE>& Vector2<TYPE>::operator*=(const TYPE aRight)
+	{
+		x *= aRight;
+		y *= aRight;
+
+		return self;
+	}
+
+	template<typename TYPE>
+	Vector2<TYPE>& Vector2<TYPE>::operator/=(const TYPE aRight)
+	{
+		x /= aRight;
+		y /= aRight;
+
+		return self;
+	}
+
+	template<typename TYPE>
+	Vector2<TYPE> Vector2<TYPE>::operator*(const TYPE aRight) const
+	{
+		Vector2 left = self;
+		left *= aRight;
+
+		return left;
+	}
+
+	template<typename TYPE>
+	Vector2<TYPE> Vector2<TYPE>::operator/(const TYPE aRight) const
+	{
+		Vector2 left = self;
+		left /= aRight;
+
+		return left;
+	}
+
+	template<typename TYPE>
+	bool Vector2<TYPE>::operator==(const Vector2& aRight) const
+	{
+		return (x == aRight.x) && (y == aRight.y);
+	}
+
+	template<typename TYPE>
+	bool Vector2<TYPE>::operator!=(const Vector2& aRight) const
+	{
+		return !(self == aRight);
+	}
+
+	template<typename TYPE>
+	TYPE Vector2<TYPE>::Length() const
+	{
+		return sqrt(Length2());
+	}
+
+	template<typename TYPE>
+	TYPE Vector2<TYPE>::Length2() const
+	{
+		return (x * x) + (y * y);
+	}
+
+	template<typename TYPE>
+	TYPE Vector2<TYPE>::Dot(const Vector2<TYPE>& aRight) const
+	{
+		return (x * aRight.x) + (y * aRight.y);
+	}
+
+	template<typename TYPE>
+	TYPE Vector2<TYPE>::Cross(Vector2<TYPE> aVector) const
+	{
+		return (x * aVector.y - y * aVector.x);
+	}
+
+	template<typename TYPE>
+	Vector2<TYPE> Vector2<TYPE>::GetNormalized() const
+	{
+		Vector2 normalized = self;
+		normalized.Normalize();
+
+		return normalized;
+	}
+
+	template<typename TYPE>
+	Vector2<TYPE>& Vector2<TYPE>::Normalize()
+	{
+		TYPE length = Length();
+		if (length != static_cast<TYPE>(0))
+		{
+			self /= length;
+		}
+
+		return self;
 	}
 }
 
-#undef VECTOR_TEMPLATE_DECL
+#undef self
