87e08058ea502869ad9e7a74dbd60b3755da7687
Alex Pettersson <alpe0504@gmail.com>
2016-12-16 14:03:45 +0100

    Started on a Console command suggester

--- Source/BrontosaurusEngine/Console.cpp
+++ Source/BrontosaurusEngine/Console.cpp
@@ -23,8 +23,36 @@ void CConsole::GetLuaFunctions()
 
 void CConsole::Activate()
 {
+	//ta all input
+	//Börja Renderas
 }
 
 void CConsole::Deactivate()
 {
+	//sluta ta all input
+	//sluta renderas
+}
+
+void CConsole::Update()
+{
+}
+
+void CConsole::Render()
+{
+}
+
+const CU::GrowingArray<std::string>& CConsole::UpdateCommandSuggestions(const std::string & aStringToCompare)
+{
+	CU::GrowingArray<std::string> mySuggestedCommands;
+	mySuggestedCommands.Init(myLuaFunctions.count);
+	std::map<std::string, SSlua::LuaCallbackFunction>::iterator it;
+	for (it = myLuaFunctions.begin(); it != myLuaFunctions.end(); it++)
+	{
+		resultDifference = YouCanTSpell(aStringToCompare, it->first);
+		if (resultDifference < difference)
+		{
+			difference = resultDifference;
+			didYouMean = it->first;
+		}
+	}
 }
--- Source/BrontosaurusEngine/Console.h
+++ Source/BrontosaurusEngine/Console.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <map>
 #include "../LuaWrapper/SSlua/SSlua.h"
+#include "../CommonUtilities/GrowingArray.h"
 
 class CConsole
 {
@@ -12,8 +13,20 @@ public:
 	void GetLuaFunctions();
 	void Activate();
 	void Deactivate();
+	
+
+	void Update();
+	void Render();
+
+private:
+	const CU::GrowingArray<std::string>& UpdateCommandSuggestions(const std::string& aStringToCompare);
+
+
+
 
 private:
 	std::map<std::string, SSlua::LuaCallbackFunction> myLuaFunctions;
+	CU::GrowingArray<std::string> mySuggestedCommands;
+
 };
 
