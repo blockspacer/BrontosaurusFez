6b56318b9929d7e72724d27e4607d7c12416471e
MarcusM <marcus.malmqvist.sp15@thegameassembly.com>
2016-12-19 14:33:21 +0100

    Input controller now sends input to activate skills in skillsystem.

--- Source/Components/Skill.h
+++ Source/Components/Skill.h
@@ -5,9 +5,19 @@ class Skill
 public:
 	Skill();
 	~Skill();
-	virtual void OnActivation();
-
+	virtual void Activate();
+	virtual void Deactivate();
+	virtual void Update(float aDeltaTime);
+	inline virtual bool GetIsActive();
 protected:
-
+	virtual void OnActivation();
+	virtual void OnDeActivation();
+	
+	bool myIsActive;
 };
 
+
+inline bool Skill::GetIsActive()
+{
+	return myIsActive;
+}
--- Source/Components/SkillSystemComponent.cpp
+++ Source/Components/SkillSystemComponent.cpp
@@ -1,13 +1,51 @@
 #include "stdafx.h"
 #include "SkillSystemComponent.h"
+#include "Skill.h"
 
-
+#include <iostream>
 SkillSystemComponent::SkillSystemComponent()
 {
 	mySkills.Init(4);
+	myTarget = nullptr;
 }
 
 
 SkillSystemComponent::~SkillSystemComponent()
+{
+}
+
+void SkillSystemComponent::Update(float aDeltaTime)
+{
+	for(unsigned short i = 0;  i < mySkills.Size(); i++)
+	{
+		if(mySkills[i]->GetIsActive() == true)
+		{
+			mySkills[i]->Update(aDeltaTime);
+
+		}
+	}
+}
+
+void SkillSystemComponent::Receive(const eComponentMessageType aMessageType, const SComponentMessageData & aMessageData)
+{
+	if(aMessageType == eComponentMessageType::eUseSkill)
+	{
+		if(aMessageData.myInt < mySkills.Size())
+		{
+			mySkills[aMessageData.myInt]->Activate();
+		}
+		else
+		{
+			std::cout << "Tried to use skill that was not in skillSystemComponent" << std::endl;
+		}
+	
+	}
+	else if(aMessageType == eComponentMessageType::eSetSkillTarget)
+	{
+		myTarget = aMessageData.myGameObject;
+	}
+}
+
+void SkillSystemComponent::Destroy()
 {
 }
