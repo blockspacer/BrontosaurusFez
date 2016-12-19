74decdd4ae83a6d5888cf78d9b5e077ed80f041c
Kyle <kyle.thorburn.sp15@thegameassembly.com>
2016-12-19 10:50:51 +0100

    denna kom inte med. Bara en kommentar.

--- Source/Components/InventoryComponentManager.h
+++ Source/Components/InventoryComponentManager.h
@@ -16,7 +16,7 @@ private:
 	~CInventoryComponentManager();
 
 	CU::GrowingArray<CInventoryComponent*> myComponents; //varför behöver jag inte forward declare'a growing-array, wtf?
-
+										// Är allt i stdafx forward declare'at som default, även om man inte inkluderar något?
 	static CInventoryComponentManager* ourInstance;
 };
 
