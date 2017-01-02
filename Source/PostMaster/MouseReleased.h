972aeac25c755513167feb8721bf1e4c2f424c97
carlberyeur <carlcarlberger@gmail.com>
2017-01-02 16:47:23 +0100

    Removed mouse down event :O moved stuff to update in input controller

--- Source/PostMaster/MouseReleased.h
+++ Source/PostMaster/MouseReleased.h
@@ -13,8 +13,9 @@ public:
 	MouseReleased(const CU::Vector2f& aMousePosition, const CU::eMouseButtons& aMouseButton);
 	~MouseReleased();
 
-	eMessageReturn DoEvent(GUI::GUIManager* aGUIManager) const;
+	eMessageReturn DoEvent(GUI::GUIManager* aGUIManager) const override;
 	eMessageReturn DoEvent(LevelSelectState* aLevelSelectState) const override;
+	eMessageReturn DoEvent(InputController* aInputController) const override;
 
 private:
 	const CU::Vector2f myMousePosition;
