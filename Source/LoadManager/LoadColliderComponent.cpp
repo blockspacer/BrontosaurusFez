1fd4a30e4f819e2454798f4ed1b6c45737df528e
Alex Pettersson <alpe0504@gmail.com>
2017-01-05 15:06:23 +0100

    Added base for loading Colliders

---
+++ Source/LoadManager/LoadColliderComponent.cpp
@@ -0,0 +1,21 @@
+#include "stdafx.h"
+#include "LoadColliderComponent.h"
+#include "KevinLoader/KevinLoader.h"
+#include "LoadManager.h"
+#include "ComponentManager.h"
+
+int LoadSphereCollider(KLoader::SLoadedComponentData someData)
+{
+
+	return 0;
+}
+
+int LoadBoxCollider(KLoader::SLoadedComponentData someData)
+{
+	return 0;
+}
+
+int LoadPointCollider(KLoader::SLoadedComponentData someData)
+{
+	return 0;
+}
