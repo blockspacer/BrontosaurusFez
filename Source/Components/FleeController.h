3817166150f1dd543ff8c1a60e9f4065a7b50ddd
Kyle <kyle.thorburn.sp15@thegameassembly.com>
2017-01-05 16:11:59 +0100

    Flee controller

---
+++ Source/Components/FleeController.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "IController.h"
+
+class CGameObject;
+
+class CFleeController : public IController
+{
+public:
+	CFleeController();
+	~CFleeController();
+	const CU::Vector2f Update(const CU::Time & aDeltaTime) override;
+
+	void SetWeight(const float aWeight);
+	void SetTargetsToAvoid( CU::GrowingArray<CGameObject*>* aTargetList);
+
+private:
+	void Destroy() override;
+	CU::GrowingArray<CGameObject*>* myObjectsToAvoid;
+};
+
