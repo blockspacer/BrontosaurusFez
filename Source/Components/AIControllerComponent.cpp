7100c67eb8a37a1d2757d0fb23a91009131971b6
Kyle <kyle.thorburn.sp15@thegameassembly.com>
2017-01-06 16:41:05 +0100

    The velocity in AIcontrollerManager needs to be reset before each update, otherwise Fleeing never stops, even when returning 0.

--- Source/Components/AIControllerComponent.cpp
+++ Source/Components/AIControllerComponent.cpp
@@ -3,11 +3,13 @@
 #include "GameObject.h"
 
 const CU::Vector2f StopVector = CU::Vector2f(99999, 99999);
+#define SCALAR 10 // due to being our game being so large.
 
 CAIControllerComponent::CAIControllerComponent()
 {
 	myControllers.Init(2);
 	myMaxVelocity = 70;
+	myVelocity = { 0.f,0.f };
 }
 
 
@@ -37,6 +39,7 @@ const CU::Vector2f & CAIControllerComponent::GetVelocity()
 void CAIControllerComponent::Update(const CU::Time& aDeltaTime)
 {
 	CU::Vector2f Acceleration = CU::Vector2f::Zero;
+	myVelocity = CU::Vector2f::Zero;
 	for (unsigned int i = 0; i < myControllers.Size(); ++i)
 	{
 		CU::Vector2f returnValue = myControllers[i]->Update(aDeltaTime);
@@ -47,6 +50,7 @@ void CAIControllerComponent::Update(const CU::Time& aDeltaTime)
 		}
 		Acceleration += returnValue;
 	}
+
 	myVelocity += Acceleration * aDeltaTime.GetSeconds();
 
 	if (myVelocity.Length() > myMaxVelocity)
@@ -55,7 +59,7 @@ void CAIControllerComponent::Update(const CU::Time& aDeltaTime)
 		myVelocity *= myMaxVelocity;
 	}
 
-	CU::Vector2f velocity = myVelocity * aDeltaTime.GetSeconds();
+	CU::Vector2f velocity = (myVelocity * SCALAR) * aDeltaTime.GetSeconds() ;
 
 	GetParent()->GetLocalTransform().Move(CU::Vector3f(velocity.x,0,velocity.y));
 	GetParent()->NotifyComponents(eComponentMessageType::eMoving, SComponentMessageData());
