496d1284bd182f5affe60d88486b0b2ea1c23071
MarcusM <marcus.malmqvist.sp15@thegameassembly.com>
2017-01-07 14:38:09 +0100

    Added DropComponent and its manager

---
+++ Source/Components/DropComponentManager.cpp
@@ -0,0 +1,55 @@
+#include "stdafx.h"
+#include "DropComponentManager.h"
+#include"ComponentManager.h"
+#include "DropComponent.h"
+
+DropComponentManager::DropComponentManager()
+{
+	myComponents.Init(100);
+}
+
+
+DropComponentManager::~DropComponentManager()
+{
+}
+
+DropComponentManager & DropComponentManager::GetInstance()
+{
+	assert(ourInstance != nullptr && "SkillComponentManager not created (is NULL)");
+
+	return *ourInstance;
+}
+
+void DropComponentManager::CreateInstance()
+{
+	assert(ourInstance == nullptr && "SkillComponentManager already created");
+	ourInstance = new DropComponentManager();
+}
+
+void DropComponentManager::DestroyInstance()
+{
+
+	assert(ourInstance != nullptr && "SkillComponentManager not created (is NULL)");
+	ourInstance->myComponents.Destroy();
+	SAFE_DELETE(ourInstance);
+}
+
+DropComponent* DropComponentManager::CreateAndRegisterComponent()
+{
+	DropComponent* Component;
+	myComponents.Add(Component);
+	CComponentManager::GetInstance().RegisterComponent(Component);
+
+	return Component;
+}
+
+void DropComponentManager::Update(const CU::Time & aDeltaTime)
+{
+	for (DropComponent* component : myComponents)
+	{
+		if (component != nullptr)
+		{
+			component->Update(aDeltaTime.GetSeconds());
+		}
+	}
+}
---
+++ Source/Components/DropComponentManager.h
@@ -0,0 +1,20 @@
+#pragma once
+class DropComponent;
+class DropComponentManager
+{
+public:
+	static DropComponentManager& GetInstance();
+	static void CreateInstance();
+	static void DestroyInstance();
+
+	DropComponent* CreateAndRegisterComponent();
+	void Update(const CU::Time& aDeltaTime);
+private:
+	DropComponentManager();
+	~DropComponentManager();
+
+
+	CU::GrowingArray<DropComponent*> myComponents;
+	static DropComponentManager* ourInstance;
+};
+
