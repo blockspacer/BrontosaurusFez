acf4b79e5cf4bd10e5a136c5312ef9bb8283425b
Edvin Jacobsson Pontén <resanderum@gmail.com>
2017-01-09 18:40:03 +0100

    Rendercamera inits with Ortho or Projective

--- Source/BrontosaurusEngine/RenderCamera.h
+++ Source/BrontosaurusEngine/RenderCamera.h
@@ -10,19 +10,16 @@ public:
 	CRenderCamera();
 	~CRenderCamera();
 
-
-	// WHAT IS NEEDED FOR INIT OF CAMERA?
-
-	void Init(const float aFov, const float aWidth, const float aHeight, const float aFar, const float aNear, DXGI_FORMAT aFormat = DXGI_FORMAT::DXGI_FORMAT_R8G8B8A8_UNORM);
-	void Init(const float aWidth, const float aHeight, const float aFar, const float aNear, DXGI_FORMAT aFormat = DXGI_FORMAT::DXGI_FORMAT_R8G8B8A8_UNORM);
-
-
+	void InitPerspective(const float aFov, const float aWidth, const float aHeight, const float aFar, const float aNear, DXGI_FORMAT aFormat = DXGI_FORMAT::DXGI_FORMAT_R8G8B8A8_UNORM);
+	void InitOrthographic(const float aWidth, const float aHeight, const float aFar, const float aNear, const int aTextureWidth, const int aTextureHeight, DXGI_FORMAT aFormat = DXGI_FORMAT::DXGI_FORMAT_R8G8B8A8_UNORM);
+	
 	inline void SetCamera(const CU::Camera& aCamera);
-	inline const CU::Camera& GetCamera() const ;
+	inline CU::Camera& GetCamera();
 
 	inline const CRenderPackage& GetRenderPackage() const;
 
-	inline void AddRenderMessage(SRenderMessage* aRenderMessage);
+
+	void AddRenderMessage(SRenderMessage* aRenderMessage);
 	void Render();
 
 private:
@@ -36,17 +33,12 @@ inline const CRenderPackage & CRenderCamera::GetRenderPackage() const
 	return myRenderPackage;
 }
 
-inline void CRenderCamera::AddRenderMessage(SRenderMessage * aRenderMessage)
-{
-	myRenderQueue.Add(aRenderMessage);
-}
-
 inline void CRenderCamera::SetCamera(const CU::Camera & aCamera)
 {
 	myCamera = aCamera;
 }
 
-inline const CU::Camera & CRenderCamera::GetCamera() const
+inline CU::Camera & CRenderCamera::GetCamera()
 {
 	return myCamera;
 }
