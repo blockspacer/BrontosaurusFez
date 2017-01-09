70ef0ddcb6e1910a0b28cd6ebb3e51f0cf26b5cd
Alex Pettersson <alpe0504@gmail.com>
2017-01-09 10:03:21 +0100

    input controller gets saved to polling station to be able to get the player object

--- Source/Game/PollingStation.h
+++ Source/Game/PollingStation.h
@@ -5,11 +5,12 @@ class CollisionManager;
 template <typename ValueType>
 class ValueObserver;
 class WorldCenterComponent;
+class CComponent;
 
 struct PollingStation
 {
 	static void NullifyLevelSpecificData();
-
+	static CComponent* PlayerInput;
 	static CGameObject* playerObject;
 	static ValueObserver<int>* playerHealthBar;
 	static ValueObserver<int>* playerBoostBar;
