d1293fe853be6d80f2c8bb8d5bdab68307586079
Kyle <kyle.thorburn.sp15@thegameassembly.com>
2017-01-10 16:18:02 +0100

    Manager for healthbars

---
+++ Source/Components/HealthBarComponentManager.cpp
@@ -0,0 +1,30 @@
+#include "stdafx.h"
+#include "HealthBarComponentManager.h"
+
+CHealthBarComponentManager::CHealthBarComponentManager()
+{
+	myHealthbars.Init(64);
+}
+
+CHealthBarComponentManager::~CHealthBarComponentManager()
+{
+}
+
+CHealthBarComponent* CHealthBarComponentManager::GetHealthBarAt(short aIndex)
+{
+	return myHealthbars.At(aIndex);
+}
+
+CHealthBarComponent* CHealthBarComponentManager::CreateHealthbar()
+{
+	myHealthbars.Add(new CHealthBarComponent());
+	return myHealthbars.GetLast();
+}
+
+void CHealthBarComponentManager::Render()
+{
+	for (int i = 0; i < myHealthbars.Size(); ++i)
+	{
+		myHealthbars[i]->Render();
+	}
+}
