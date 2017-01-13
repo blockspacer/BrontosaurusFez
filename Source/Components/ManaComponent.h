be1b4792ef39a9e7c356aa746614e2ed3fee2ca8
MarcusM <marcus.malmqvist.sp15@thegameassembly.com>
2017-01-13 10:41:47 +0100

    Added ManaComponent and manapoint not used yet though.

---
+++ Source/Components/ManaComponent.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Component.h"
+#include "../PostMaster/Subscriber.h"
+#include "ManaPoint.h"
+
+class ManaComponent : public CComponent
+{
+public:
+	ManaComponent();
+	~ManaComponent();
+	float GetPrecentLeft() const;
+
+	void SetMana(const ManaPoint aValue);
+	void SetMaxMana(const ManaPoint aValue);
+	void Init();
+
+	void Receive(const eComponentMessageType aMessageType, const SComponentMessageData& aMessageData) override;
+	void Destroy() override;
+private:
+	float myPercentageLeft;
+	ManaPoint myMana;
+	ManaPoint myMaxMana;
+
+};
+
