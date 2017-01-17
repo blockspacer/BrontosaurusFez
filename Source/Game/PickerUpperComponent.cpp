58fb15bc9a0f76b635a809914670b2aebd14865d
Alex Pettersson <alpe0504@gmail.com>
2017-01-17 17:00:28 +0100

    Added first iteration to pickups(health,mana,gold)

---
+++ Source/Game/PickerUpperComponent.cpp
@@ -0,0 +1,56 @@
+#include "stdafx.h"
+#include "PickerUpperComponent.h"
+#include "PickupManager.h"
+#include "PickupComponent.h"
+#include "PostMaster/PostMaster.h"
+#include "PostMaster/Message.h"
+#include "../Components/GameObject.h"
+#include "../Components/ComponentMessage.h"
+
+CPickerUpperComponent::~CPickerUpperComponent()
+{
+}
+
+void CPickerUpperComponent::Receive(const eComponentMessageType aMessageType, const SComponentMessageData& aMessageData)
+{
+	if (aMessageType == eComponentMessageType::ePickUp)
+	{
+		CPickupComponent* pickupComponent = aMessageData.myPickupComponent;
+		const SPickupData data = CPickupManager::GetInstance().GetPickupData(pickupComponent);
+		SComponentMessageData messageData;
+		switch (data.myType)
+		{
+		case ePickupType::HEALTH:
+			{
+				
+				messageData.myInt = data.myValue;
+				
+				GetParent()->NotifyComponents(eComponentMessageType::eHeal, messageData);
+			}
+			break;
+		case ePickupType::MANA:
+			{
+				messageData.myInt = data.myValue;
+
+				GetParent()->NotifyComponents(eComponentMessageType::eRestoreMana, messageData);
+			}
+			break;
+		default: break;
+		}
+
+		SComponentMessageData messageData2;
+		messageData2.myBool = false;
+		pickupComponent->GetParent()->NotifyComponents(eComponentMessageType::eSetVisibility, messageData2);
+		pickupComponent->GetParent()->NotifyComponents(eComponentMessageType::eSetIsColliderActive, messageData2);
+
+	}
+}
+
+void CPickerUpperComponent::Destroy()
+{
+	myManager.DestroyPickerUpperComp(this);
+}
+
+CPickerUpperComponent::CPickerUpperComponent(CPickupManager& aManager): myManager(aManager)
+{
+}
---
+++ Source/Game/PickupComponent.cpp
@@ -0,0 +1,46 @@
+#include "stdafx.h"
+#include "PickupComponent.h"
+#include "PickupManager.h"
+#include "../Components/GameObject.h"
+#include "PollingStation.h"
+#include "../Components/ComponentMessage.h"
+//#include <iostream>
+
+void CPickupComponent::Destroy()
+{
+	myManager.DestroyPickupComp(this);
+}
+
+void CPickupComponent::Receive(const eComponentMessageType aMessageType, const SComponentMessageData& aMessageData)
+{
+	if (aMessageType == eComponentMessageType::eCollision)
+	{
+		GetParent()->NotifyComponents(eComponentMessageType::eObjectiveCompleted, SComponentMessageData());
+		SComponentMessageData data;
+		data.myPickupComponent = this;
+		aMessageData.myComponents[0]->GetParent()->NotifyComponents(eComponentMessageType::ePickUp, data);
+		return;
+	}
+}
+
+CPickupComponent::CPickupComponent(CPickupManager& aManager) : myManager(aManager)
+{
+	mySuckUpRadius = 10000.0f;
+	mySpeed = 1000.0f;
+}
+
+CPickupComponent::~CPickupComponent()
+{
+}
+
+void CPickupComponent::Update(float aDeltaTime)
+{
+	float distance2 = CU::Vector3f(PollingStation::playerObject->GetToWorldTransform().GetPosition() - GetParent()->GetToWorldTransform().GetPosition()).Length2();
+	if(distance2 < mySuckUpRadius * mySuckUpRadius)
+	{
+		CU::Matrix33f rotationMatrix = GetParent()->GetLocalTransform().GetRotation();
+		rotationMatrix.LookAt(GetParent()->GetToWorldTransform().GetPosition(), PollingStation::playerObject->GetToWorldTransform().GetPosition());
+		GetParent()->GetLocalTransform().SetPosition(CU::Vector3f(0.0f, 0.0f, mySpeed * aDeltaTime) * rotationMatrix + GetParent()->GetToWorldTransform().GetPosition());
+		GetParent()->NotifyComponents(eComponentMessageType::eMoving, SComponentMessageData());
+	}
+}
