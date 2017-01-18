b75fb5bb0d92671722029c8a2c005315835a7895
Alex Pettersson <alpe0504@gmail.com>
2017-01-18 17:23:46 +0100

    added so that gold gets dropped too

--- Source/Game/PickupFactory.cpp
+++ Source/Game/PickupFactory.cpp
@@ -67,6 +67,37 @@ void CPickupFactory::CreateHealthGlobe(CU::Vector3f aPosition)
 	myComponents.Add(collider);
 }
 
+void CPickupFactory::CreateGoldPickup(CU::Vector3f aPosition, const unsigned int aAmountToDrop)
+{
+	CGameObject* healthGlobe = myGameObjectManager->CreateGameObject();
+	healthGlobe->SetWorldPosition(aPosition);
+
+	CPickupComponent* pickup = CPickupManager::GetInstance().CreatePickupComponent(ePickupType::GOLD, aAmountToDrop, 100, 200);
+
+	healthGlobe->AddComponent(pickup);
+
+	Intersection::CollisionData CollisionData;
+
+	CollisionData.myCircleData = new Intersection::SCircle;
+	CollisionData.myCircleData->myCenterPosition = aPosition;
+	CollisionData.myCircleData->myRadius = 40; //should not be hard coded, maybe not a problem
+
+	CCollisionComponent* collider = myCollisionComponentManager->CreateCollisionComponent(CCollisionComponentManager::eColliderType::eCircle, CollisionData);
+
+	collider->AddCollidsWith(eColliderType::eColliderType_Player);
+	collider->SetColliderType(eColliderType::eColliderType_PickUp);
+
+	healthGlobe->AddComponent(collider);
+
+	CModelComponent* model = CModelComponentManager::GetInstance().CreateComponent("Models/Placeholders/goldCoins.fbx");
+
+	healthGlobe->AddComponent(model);
+
+	myObjects.Add(healthGlobe);
+	myComponents.Add(pickup);
+	myComponents.Add(collider);
+}
+
 void CPickupFactory::Init()
 {
 	//read from json doc and set modelpath and other things
