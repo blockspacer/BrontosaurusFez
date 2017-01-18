acf8938b3acf84d6d4b0914fdb9c368d35adcc0a
Alex Pettersson <alpe0504@gmail.com>
2017-01-18 14:03:07 +0100

    Updated drop component to drop health globes and added unity support for pickup component

--- Source/Game/PickupFactory.h
+++ Source/Game/PickupFactory.h
@@ -1,8 +1,39 @@
 #pragma once
-class PickupFactory
+#include "../CommonUtilities/vector3.h"
+
+
+class CGameObjectManager;
+class CCollisionComponentManager;
+class CComponent;
+class CGameObject;
+
+class CPickupFactory
 {
 public:
-	PickupFactory();
-	~PickupFactory();
+	static void Create(CGameObjectManager* aCGameObjectManager, CCollisionComponentManager* aManager);
+	static void Destroy();
+	static CPickupFactory& GetInstance();
+
+
+	void CreateHealthGlobe(CU::Vector3f aPosition);
+
+private:
+	void Init();
+
+	CPickupFactory(CGameObjectManager* aCGameObjectManager, CCollisionComponentManager* aManager);
+	~CPickupFactory();
+
+
+
+private:
+
+	CU::GrowingArray<CGameObject*> myObjects;
+	CU::GrowingArray<CComponent*> myComponents;
+
+	static CPickupFactory* ourInstance;
+	CGameObjectManager* myGameObjectManager;
+	CCollisionComponentManager* myCollisionComponentManager;
+	std::string myModelPath;
+
 };
 
