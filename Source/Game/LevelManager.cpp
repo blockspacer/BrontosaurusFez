8f1c5985ac10cfab1d640e72bf2dd8a2d9cd4028
Kyle <kyle.thorburn.sp15@thegameassembly.com>
2017-01-23 10:08:13 +0100

    created Level Mgr

---
+++ Source/Game/LevelManager.cpp
@@ -0,0 +1,31 @@
+#include "stdafx.h"
+#include <assert.h>
+#include "LevelManager.h"
+#include "..\PostMaster\PostMaster.h"
+#include "..\PostMaster\ChangeLevel.h"
+#include "..\PostMaster\Message.h"
+#include <CommonUtilities.h>
+
+CLevelManager* CLevelManager::ourInstance = nullptr;
+
+void CLevelManager::CreateInstance()
+{
+	assert(ourInstance == nullptr && "LevelMgr already exists");
+	ourInstance = new CLevelManager();
+}
+
+void CLevelManager::DestroyInstance()
+{
+	SAFE_DELETE(ourInstance);
+}
+
+CLevelManager* CLevelManager::GetInstance()
+{
+	assert(ourInstance != nullptr && "LevelMgr doesn't exists");
+	return ourInstance;
+}
+
+void CLevelManager::GoToLevel(unsigned char aIndex)
+{
+	PostMaster::GetInstance().SendLetter(Message(eMessageType::eStateStackMessage, ChangeLevel(aIndex)));
+}
