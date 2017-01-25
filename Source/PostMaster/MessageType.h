53ef9b8d3f7fbc960349e87a410a204d4e782bd6
MarcusM <marcus.malmqvist.sp15@thegameassembly.com>
2017-01-25 11:00:33 +0100

    Added GameObjectDiedEvent Event Event

--- Source/PostMaster/MessageType.h
+++ Source/PostMaster/MessageType.h
@@ -31,6 +31,7 @@ enum class eMessageType
 	ePlayerHealthChanged,
 	ePlayerManaChanged,
 	eShopItemSelected,
+	eGameObjectDied,
 	eLength,
 };
 //Ifall du vill lägga till en till messagetyp så kompilera om Postmaster Projekter annars kraschar det. 
