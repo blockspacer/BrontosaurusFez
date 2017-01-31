3cc55060f25e23f498db00c3643d1fe51eb0f327
carlberyeur <carlcarlberger@gmail.com>
2017-01-31 19:45:44 +0100

    removed hardcoding in load fire component

--- Source/LoadManager/LoadFireComponent.cpp
+++ Source/LoadManager/LoadFireComponent.cpp
@@ -12,29 +12,29 @@ int LoadFireComponent(KLoader::SLoadedComponentData someData)
 
 	CFireEmitterInstance fireEmitter;
 	SFireEmitterData fireData;
-	fireData.myScrollSpeeds[0] = 1.3f; //someData.myData["ScrollSpeeds"][0].GetFloat();
-	fireData.myScrollSpeeds[1] = 2.1f; //someData.myData["ScrollSpeeds"][1].GetFloat();
-	fireData.myScrollSpeeds[2] = 2.3f; //someData.myData["ScrollSpeeds"][2].GetFloat();
+	fireData.myScrollSpeeds[0] = /*1.3f;*/someData.myData["ScrollSpeeds"][0].GetFloat();
+	fireData.myScrollSpeeds[1] = /*2.1f;*/ someData.myData["ScrollSpeeds"][1].GetFloat();
+	fireData.myScrollSpeeds[2] = /*2.3f;*/ someData.myData["ScrollSpeeds"][2].GetFloat();
 
-	fireData.myScales[0] = 1.f; //someData.myData["Scales"][0].GetFloat();
-	fireData.myScales[1] = 2.f; //someData.myData["Scales"][1].GetFloat();
-	fireData.myScales[2] = 3.f; //someData.myData["Scales"][2].GetFloat();
+	fireData.myScales[0] = /*1.f;*/ someData.myData["Scales"][0].GetFloat();
+	fireData.myScales[1] = /*2.f;*/ someData.myData["Scales"][1].GetFloat();
+	fireData.myScales[2] = /*3.f;*/ someData.myData["Scales"][2].GetFloat();
 
-	//CU::CJsonValue distortions = someData.myData["Distortions"];
+	CU::CJsonValue distortions = someData.myData["Distortions"];
 
-	fireData.myDistortions[0].Set(0.1f, 0.2f); //distortions[0]["x"].GetFloat(); distortions[0]["y"].GetFloat();
-	fireData.myDistortions[1].Set(0.1f, 0.3f); //distortions[1]["x"].GetFloat(); distortions[1]["y"].GetFloat();
-	fireData.myDistortions[2].Set(0.1f, 0.1f); //distortions[2]["x"].GetFloat(); distortions[2]["y"].GetFloat();
+	fireData.myDistortions[0].Set(/*0.1f, 0.2f);*/ distortions[0]["x"].GetFloat(), distortions[0]["y"].GetFloat());
+	fireData.myDistortions[1].Set(/*0.1f, 0.3f);*/ distortions[1]["x"].GetFloat(), distortions[1]["y"].GetFloat());
+	fireData.myDistortions[2].Set(/*0.1f, 0.1f);*/ distortions[2]["x"].GetFloat(), distortions[2]["y"].GetFloat());
 
-	fireData.myDistortionScale = 0.8f;
-	fireData.myDistortionBias = 0.5f;
+	fireData.myDistortionScale = /*0.8f*/ someData.myData["DistortionScale"].GetFloat();
+	fireData.myDistortionBias = /*0.5f*/ someData.myData["DistortionBias"].GetFloat();
 
 	fireEmitter.Init(fireData);
-	fireEmitter.GetTransformation().Rotate(3.141592f, CU::Axees::Y); //someData.myData["RotationY"].GetFloat();
-	//float scale = someData.myData["Scale"].GetFloat();
-	fireEmitter.GetTransformation().m11 *= 2.f; //scale;
-	fireEmitter.GetTransformation().m22 *= 2.f; //scale;
-	fireEmitter.GetTransformation().m33 *= 2.f; //scale;
+	fireEmitter.GetTransformation().Rotate(/*3.141592f*/someData.myData["RotationY"].GetFloat(), CU::Axees::Y);
+	float scale = someData.myData["Scale"].GetFloat();
+	fireEmitter.GetTransformation().m11 *= /*2.f;*/ scale;
+	fireEmitter.GetTransformation().m22 *= /*2.f;*/ scale;
+	fireEmitter.GetTransformation().m33 *= /*2.f;*/ scale;
 
 	InstanceID id = scene->AddFireEmitters(fireEmitter);
 
