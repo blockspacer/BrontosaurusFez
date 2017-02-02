1c8daaa0c5b8b83ec41b6aa11cb3c21713e35211
MarcusM <marcus.malmqvist.sp15@thegameassembly.com>
2017-02-02 15:37:47 +0100

    Fixed so that witch can use new animations

--- Source/BrontosaurusEngine/ModelManager.cpp
+++ Source/BrontosaurusEngine/ModelManager.cpp
@@ -213,8 +213,8 @@ void CModelManager::LoadAnimations(const char* aPath, const ModelId aModelId)
 	modelName -= std::string(".fbx");
 	modelName += std::string("_");
 
-	const ModelId animationCount = 7;
-	const std::string animationNames[animationCount] = { ("idle"), ("idle2"), ("walk"), ("pickup"), ("turnRight90"), ("turnLeft90"), ("attack") };
+	const ModelId animationCount = 9;
+	const std::string animationNames[animationCount] = { ("idle"), ("idle2"), ("walk"), ("pickup"), ("turnRight90"), ("turnLeft90"), ("attack") , ("summon"), ("died") };
 
 	CModel* mdl = GetModel(aModelId);
 	const aiScene* scene = mdl->GetScene();
