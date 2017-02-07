449a6f8b3b7b41f258605c840c90ab1d9c8c62ed
carlberyeur <carlcarlberger@gmail.com>
2017-02-07 19:30:59 +0100

    get the name of the thread you are on with CU::GetThreadName(), or send in thread id or std::thread& to get its name

--- Source/CommonUtilities/ThreadNamer.h
+++ Source/CommonUtilities/ThreadNamer.h
@@ -12,4 +12,8 @@ namespace CU
 	void SetThreadName(const char* threadName);
 
 	void SetThreadName(std::thread& thread, const char* threadName);
+
+	const std::string& GetThreadName();
+	const std::string& GetThreadName(const std::thread& aThread);
+	const std::string& GetThreadName(const uint32_t aThreadID);
 }
