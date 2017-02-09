ba82676d2d30ca03fd65b63aa32eaad457eb9ec5
Alex Pettersson <alpe0504@gmail.com>
2017-02-09 11:57:07 +0100

    Launcher update

--- Source/RealLauncher/Form1.Designer.cs
+++ Source/RealLauncher/Form1.Designer.cs
@@ -106,7 +106,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Decaying Orbit";
+            this.Text = "Hat N\' Slash";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
